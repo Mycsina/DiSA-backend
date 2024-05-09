1fba20a8025dd58ae7caa1047444291f04c57fb0
Maria Joao <mariasardinha@ua.pt>
2024-05-09 14:27:08 +0100

    logging done

--- routes/documents.py
+++ routes/documents.py
@@ -1,3 +1,4 @@
+import logging
 from sqlite3 import IntegrityError
 from typing import Annotated
 from uuid import UUID
@@ -10,6 +11,9 @@ from utils.exceptions import IntegrityBreach
 from models.user import User
 from utils.security import get_current_user
 from storage.main import engine
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 documents_router = APIRouter(
     prefix="/documents",
@@ -25,25 +29,41 @@ async def update_document(
     file: UploadFile,
 ):
     with Session(engine) as session:
-        col = collections.get_collection_by_id(session, col_uuid, user)
-        doc = collections.get_document_by_id(session, doc_uuid)
-
-        if col is None:
-            raise HTTPException(status_code=404, detail="Collection not found")
-        if doc is None:
-            raise HTTPException(status_code=404, detail="Document not found")
-        if doc not in col.documents:
-            raise HTTPException(status_code=404, detail="Document not in the specified collection")
-        if not col.can_write(user):
-            raise HTTPException(status_code=403, detail="You do not have permission to write to this collection")
-        data = await file.read()
         try:
+            col = collections.get_collection_by_id(session, col_uuid, user)
+            doc = collections.get_document_by_id(session, doc_uuid)
+
+            if col is None:
+                logger.error(f"Collection not found.")
+                raise HTTPException(status_code=404, detail="Collection not found")
+            if doc is None:
+                logger.error(f"Document not found.")
+                raise HTTPException(status_code=404, detail="Document not found")
+            if doc not in col.documents:
+                logger.error(f"Document not in the specified collection.")
+                raise HTTPException(status_code=404, detail="Document not in the specified collection")
+            if not col.can_write(user):
+                logger.error(f"This user does not have permission to write to this collection.")
+                raise HTTPException(status_code=403, detail="You do not have permission to write to this collection")
+            data = await file.read()
             collections.update_document(session, user, col, doc, data)
+            if doc.next is None:
+                logger.error(f"Document update failed.")
+                raise HTTPException(status_code=500, detail="Document update failed")
+            logger.info("Document updated successfully.")
+            return {"message": "Document updated successfully", "update_uuid": doc.next.id}
         except IntegrityError:
+            logger.error("Document update failed due to an integrity error.")
             raise IntegrityBreach("Document update failed. Verify the document hasn't already been updated")
-        if doc.next is None:
-            raise HTTPException(status_code=500, detail="Document update failed")
-        return {"message": "Document updated successfully", "update_uuid": doc.next.id}
+        except HTTPException as http_exception:
+            logger.error(f"HTTP Exception ocurred: {http_exception}")
+            raise http_exception
+        except Exception as e:
+            logger.error(f"An unexpected error ocurred: {e}")
+            raise HTTPException(status_code=500, detail="Internal server error")
+        
+            
+            
 
 
 @documents_router.delete("/documents/")
@@ -53,18 +73,30 @@ async def delete_document(
     doc_uuid: UUID,
 ):
     with Session(engine) as session:
-        col = collections.get_collection_by_id(session, col_uuid, user)
-        doc = collections.get_document_by_id(session, doc_uuid)
-        if col is None:
-            raise HTTPException(status_code=404, detail="Collection not found")
-        if not col.can_write(user):
-            raise HTTPException(status_code=403, detail="You do not have permission to write to this collection")
-        if doc is None:
-            raise HTTPException(status_code=404, detail="Document not found")
-        if doc not in col.documents:
-            raise HTTPException(status_code=404, detail="Document not in the specified collection")
-        collections.delete_document(session, doc)
-        return {"message": "Document deleted successfully"}
+        try:
+            col = collections.get_collection_by_id(session, col_uuid, user)
+            doc = collections.get_document_by_id(session, doc_uuid)
+            if col is None:
+                logger.error("Collection not found.")
+                raise HTTPException(status_code=404, detail="Collection not found")
+            if not col.can_write(user):
+                logger.error("The user does not have permission tp write to this collection.")
+                raise HTTPException(status_code=403, detail="You do not have permission to write to this collection")
+            if doc is None:
+                logger.error("Document not found.")
+                raise HTTPException(status_code=404, detail="Document not found")
+            if doc not in col.documents:
+                logger.error("Document not in the specified collection.")
+                raise HTTPException(status_code=404, detail="Document not in the specified collection")
+            collections.delete_document(session, doc)
+            logger.info("Document deleted successfully.")
+            return {"message": "Document deleted successfully"}
+        except HTTPException as http_exception:
+            logger.error(f"HTTP Exception occured: {http_exception}")
+            raise http_exception
+        except Exception as e:
+            logger.error(f"An unexpected error occured: {e}")
+            raise HTTPException(status_code=500, detail="Internal server error")
 
 
 @documents_router.get("/documents/search")
@@ -74,15 +106,26 @@ async def search_documents(
     name: str,
 ):
     with Session(engine) as session:
-        col = collections.get_collection_by_id(session, col_uuid, user)
-        if col is None:
-            raise HTTPException(status_code=404, detail="Collection not found")
-        if col.owner != user:
-            raise HTTPException(status_code=403, detail="You are not the owner of this Collection")
-        documents = collections.search_documents(session, col, name)
-        if documents is None:
-            raise HTTPException(status_code=404, detail="No documents found")
-        return documents
+        try:
+            col = collections.get_collection_by_id(session, col_uuid, user)
+            if col is None:
+                logger.error("Collection not found.")
+                raise HTTPException(status_code=404, detail="Collection not found")
+            if col.owner != user:
+                logger.error("This user is not the owner of this collection.")
+                raise HTTPException(status_code=403, detail="You are not the owner of this Collection")
+            documents = collections.search_documents(session, col, name)
+            if documents is None:
+                logger.error("No documents found.")
+                raise HTTPException(status_code=404, detail="No documents found")
+            logger.info("Document found successfully.")
+            return documents
+        except HTTPException as http_exception:
+            logger.error(f"HTTP Exception occured: {http_exception}")
+            raise http_exception
+        except Exception as e:
+            logger.error(f"An unexpected error occured: {e}")
+            raise HTTPException(status_code=500, detail="Internal server error")
 
 
 @documents_router.get("/documents/history")
@@ -92,14 +135,26 @@ async def get_document_history(
     doc_uuid: UUID,
 ):
     with Session(engine) as session:
-        col = collections.get_collection_by_id(session, col_uuid, user)
-        doc = collections.get_document_by_id(session, doc_uuid)
-        if col is None:
-            raise HTTPException(status_code=404, detail="Collection not found")
-        if not col.can_read(user):
-            raise HTTPException(status_code=403, detail="You do not have permission to read this collection")
-        if doc is None:
-            raise HTTPException(status_code=404, detail="Document not found")
-        if doc not in col.documents:
-            raise HTTPException(status_code=404, detail="Document not in the specified collection")
-        return collections.get_document_history(session, doc)
+        try:
+            col = collections.get_collection_by_id(session, col_uuid, user)
+            doc = collections.get_document_by_id(session, doc_uuid)
+            if col is None:
+                logger.error("Collection not found.")
+                raise HTTPException(status_code=404, detail="Collection not found")
+            if not col.can_read(user):
+                logger.error("This user does not have permission to read this collection.")
+                raise HTTPException(status_code=403, detail="You do not have permission to read this collection")
+            if doc is None:
+                logger.error("Document not found.")
+                raise HTTPException(status_code=404, detail="Document not found")
+            if doc not in col.documents:
+                logger.error("Document not found in the specified collection.")
+                raise HTTPException(status_code=404, detail="Document not in the specified collection")
+            logger.info("Document found successfully.")
+            return collections.get_document_history(session, doc)
+        except HTTPException as http_exception:
+            logger.error(f"HTTP Exception occured: {http_exception}")
+            raise http_exception
+        except Exception as e:
+            logger.error(f"An unexpected error occured: {e}")
+            raise HTTPException(status_code=500, detail="Internal server error")
